a490f08817dabbe73706f36e0860ca2ef5ad65a9
Randy-Nunez <randynunez468@gmail.com>
2025-10-03 10:35:44 -0400

    created fibonacci function

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // Prints the first n Fibonacci numbers
 const printFibonacci = (n) => {
- 
+    let firstNumber = 0;
+    let secondNumber = 1;
+    let sum;
+    for (let i = 0; i < n; i++) {
+        console.log(firstNumber)
+        sum = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = sum;
+    }
 }
 
+console.log("Example 1")
 printFibonacci(5);  // should print 0, 1, 1, 2, 3
+console.log("Example 2")
 printFibonacci(10); // should print 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
